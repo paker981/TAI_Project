c9f45f38284028b6187bfa6a78ae70a86d22d60e
paker981 <michal.a.mojkowski@wp.pl>
2023-01-26 15:13:27 +0100

    fullauthorize

--- Projekt_TAI_API/Controllers/UserController.cs
+++ Projekt_TAI_API/Controllers/UserController.cs
@@ -1,8 +1,15 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Projekt_TAI_API.Data;
+using Projekt_TAI_API.Helpers;
 using Projekt_TAI_API.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Projekt_TAI_API.Controllers
 {
@@ -22,33 +29,94 @@ namespace Projekt_TAI_API.Controllers
             if (uzytkownikrequest == null)
                 return BadRequest();
 
-            uzytkownikrequest.id = Guid.NewGuid();
+            if(await CheckEmailExistAsync(uzytkownikrequest.email))
+                return BadRequest(new {Message = "Email Already Exist!"});
 
+            var pass = CheckPasswordStrength(uzytkownikrequest.haslo);
+
+            if (!string.IsNullOrEmpty(pass))
+                return BadRequest(new { Message = pass.ToString() });
+    
+            uzytkownikrequest.id = Guid.NewGuid();
+            uzytkownikrequest.haslo = PasswordHashed.HashPassword(uzytkownikrequest.haslo);
+            uzytkownikrequest.role = "User";
+            uzytkownikrequest.token = "";
             await _fullStackDbContext.Uzytkownicy.AddAsync(uzytkownikrequest);
             await _fullStackDbContext.SaveChangesAsync();
 
             return Ok(uzytkownikrequest);
 
         }
+
+        
+
         [HttpPost("authenticate")]
         public async Task<IActionResult> Authenticate([FromBody] uzytkownik userObj)
         {
             if(userObj == null)
                 return BadRequest();
 
-            var user = await _fullStackDbContext.Uzytkownicy.FirstOrDefaultAsync(x => x.email == userObj.email && x.haslo == userObj.haslo);
+
+            var user = await _fullStackDbContext.Uzytkownicy.FirstOrDefaultAsync(x => x.email == userObj.email);
             if(user==null)
                 return NotFound(new 
                 {Message= "User Not Found!"});
 
+            if(!PasswordHashed.VerifyPassword(userObj.haslo, user.haslo))
+            {
+                return BadRequest(new { Message = "Haslo jest nieprawidłowe!" });
+            }
+
+            user.token = CreateJwt(user);
+
             return Ok(new
             {
+                Token = user.token,
                 Message = "Login Success!"
-            });
+            }); ;
         }
         
         
-        
+        private async Task<bool> CheckEmailExistAsync(string email)
+        {
+            return await _fullStackDbContext.Uzytkownicy.AnyAsync(x => x.email == email);
+        }
+
+        private string CheckPasswordStrength(string password)
+        {
+            StringBuilder sb = new StringBuilder();
+            if(password.Length < 6)
+                     sb.Append("Minimalna dlługość hasło powinna wynosić 6 znaków" + Environment.NewLine);    
+            if(!(Regex.IsMatch(password,"[a-z]") && Regex.IsMatch(password,"[A-Z]") && Regex.IsMatch(password,"[0-9]")))  
+                     sb.Append("Hasło powinno być zawierać tylko znaki Alfanumeryczne" + Environment.NewLine);
+            
+            if (!Regex.IsMatch(password,"[<,>,;,.,!,@,',[,`,~]"))
+                    sb.Append("Hasło powinno zawierać znak specjalny"+Environment.NewLine);
+
+            return sb.ToString();
+        }
+
+        private string CreateJwt(uzytkownik user) 
+        {
+            var jwtTokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("veryverysecret.....");
+            var identity = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Role, user.role),
+                new Claim(ClaimTypes.Name, $"{user.imie} {user.numer}")
+            });
+            var credentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = identity,
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = credentials
+            };
+
+            var token = jwtTokenHandler.CreateToken(tokenDescriptor);
+            return jwtTokenHandler.WriteToken(token);
+        }
 
 
 
@@ -68,5 +136,13 @@ namespace Projekt_TAI_API.Controllers
             return NotFound();
 
         }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult<uzytkownik>> GetAllUsers()
+        {
+            return Ok(await _fullStackDbContext.Uzytkownicy.ToListAsync());
+        }
+
     }
 }
--- Projekt_TAI_API/Program.cs
+++ Projekt_TAI_API/Program.cs
@@ -1,6 +1,10 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using Projekt_TAI_API.Data;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,10 +15,37 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(option =>
+{
+    option.AddPolicy("MyPolicy", builder =>
+    {
+        builder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+});
 
 builder.Services.AddDbContext<FullStackDbContext>(options =>
 options.UseNpgsql(builder.Configuration.GetConnectionString("FullStackConnectionString"))
 );
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("veryverysecret.....")),
+        ValidateAudience = false,
+        ValidateIssuer = false,
+        ClockSkew = TimeSpan.Zero
+    };
+});
 
 
 var app = builder.Build();
@@ -29,6 +60,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
